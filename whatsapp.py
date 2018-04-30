116c6a6dce4c8c6213d5f5013557b5ee0b46abae
Liam Cripwell <ljcripwell@gmail.com>
2018-04-30 11:43:50 +1000

    added some documentation

--- whatsapp.py
+++ whatsapp.py
@@ -6,18 +6,24 @@ class WhatsappAnalyzer():
         self.messages = []                
 
     def parse(self):
+        """
+        Parse each line of chat_file and identify date, user and message content from each
+        """
         import re
 
+        # regex patters for finding each component
         date_regex = "^\[(.*)\]"
         user_regex = "^\[.*\] ([^:]*):"
         msg_regex = "^\[.*\] [^:]*: (.*)$"
 
         with open(self.chat_file) as f:
             for line in f:
+                # search for components
                 date_search = re.search(date_regex, line)
                 user_search = re.search(user_regex, line)
                 msg_search = re.search(msg_regex, line)
 
+                # if found, save to memory
                 if date_search:
                     date = date_search.group(1)
                 if user_search:
@@ -25,6 +31,7 @@ class WhatsappAnalyzer():
                 if msg_search:
                     msg = msg_search.group(1)
 
+                # if validly constructed message, save to object
                 if date and user and msg:
                     post = (date, user, msg)
                     if user not in self.users:
@@ -32,6 +39,9 @@ class WhatsappAnalyzer():
                     self.messages.append(post)
 
     def message_count(self):
+        """
+        Calculate number of messages sent by each user
+        """
         for user in self.users:
             count = len([x for x in self.messages if x[1] == user])
             print("%s: %d" % (user, count))
